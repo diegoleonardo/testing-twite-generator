ce145bcd75c2e9f04d44b1b2bfd472c333b12a86
diego santos <diego@nama.ai>
2019-12-27 00:45:55 -0300

    refactor

--- TwitterStormTests/TwitesGenerator.cs
+++ TwitterStormTests/TwitesGenerator.cs
@@ -2,20 +2,20 @@ using System.Collections.Generic;
 
 namespace TwitterStormTests {
     public class TwitesGenerator {
-        private const int TwitLimit = 160;
+        private const int TwiteLimit = 160;
 
         public string[] Generate(string textToSplit) {
-            var twitesList = new List<string>();
+            var twiteList = new List<string>();
             if (string.IsNullOrWhiteSpace(textToSplit)) {
-                return twitesList.ToArray();
+                return twiteList.ToArray();
             }
             var splittedText = textToSplit.ToCharArray();
             var count = 0;
             var characters = string.Empty;
 
             for (int i = 0; i < splittedText.Length; i++) {
-                if (count == TwitLimit) {
-                    twitesList.Add(characters);
+                if (count == TwiteLimit) {
+                    twiteList.Add(characters);
                     characters = string.Empty;
                     count = 0;
                 }
@@ -23,13 +23,13 @@ namespace TwitterStormTests {
                 characters += splittedText[i];
 
                 if (i == splittedText.Length - 1) {
-                    twitesList.Add(characters);
+                    twiteList.Add(characters);
                 }
 
                 count++;
             }
 
-            return twitesList.ToArray();
+            return twiteList.ToArray();
         }
     }
 }
